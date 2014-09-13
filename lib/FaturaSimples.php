db5fb9fece4d52df1495ce9b4733458ab79f4756
Efrem Moraes <efremmoraes@users.noreply.github.com>
2014-09-13 14:11:31 -0300

    Update FaturaSimples.php

--- lib/FaturaSimples.php
+++ lib/FaturaSimples.php
@@ -10,7 +10,7 @@ foreach( array('curl_init' => "CURL", 'json_decode' => "JSON") as $function => $
 require(dirname(__FILE__) . '/FaturaSimples/FaturaSimples.php');
 require(dirname(__FILE__) . '/FaturaSimples/Venda.php');
 
-
+/*
 $params = array(
     "data" => "12/09/2014",
     "cliente" => "ERAMO SOFTWARE",
@@ -30,21 +30,21 @@ $params = array(
 
 FaturaSimples::configure("http://dev/fenix_nfse/", "2y6v8qnX3SUHZb0sanKWrQUnTAY");
 
-// $venda = FaturaSimples_Venda::criar( $params );
+$venda = FaturaSimples_Venda::criar( $params );
 
-// $venda = FaturaSimples_Venda::selecionar( 47165 );
+$venda = FaturaSimples_Venda::selecionar( 47165 );
 
-// $venda = FaturaSimples_Venda::deletar( 47163 );
+$venda = FaturaSimples_Venda::deletar( 47163 );
 
-// $params = array(
-//     'discriminacao' => 'Nova Atualizada ' . rand(0,21123321)
-// );
+$params = array(
+    'discriminacao' => 'Nova Atualizada ' . rand(0,21123321)
+);
 
-// $venda = FaturaSimples_Venda::atualizar( 47165, $params );
+$venda = FaturaSimples_Venda::atualizar( 47165, $params );
 
-// $venda = FaturaSimples_Venda::nfseCancelar( 47165, FaturaSimples_Venda::NFSE_CANCELAR_ERRO_EMISSAO );
+$venda = FaturaSimples_Venda::nfseCancelar( 47165, FaturaSimples_Venda::NFSE_CANCELAR_ERRO_EMISSAO );
 
-// $venda = FaturaSimples_Venda::listar(0, 1);
-
+$venda = FaturaSimples_Venda::listar(0, 1);
 
 echo $venda;
+*/
